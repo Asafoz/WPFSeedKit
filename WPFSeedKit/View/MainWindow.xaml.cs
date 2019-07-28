2b7b0c3bfc8db8c06aed7134c95c3ef29ad45166
Asafoz <Asafoz@users.noreply.github.com>
2019-07-28 12:22:22 +0300

    Add Files

---
+++ WPFSeedKit/View/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Bolareshet.WPFSeedKit.Interface;
+
+namespace Bolareshet.WPFSeedKit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [Export]
+    public partial class MainWindow : Window
+    {
+        [ImportingConstructor]
+        public MainWindow([Import(AllowDefault = true)]IMainWindowViewModel viewModel)
+        {
+            InitializeComponent();
+
+            this.Loaded += (s, e) =>
+            {
+                this.DataContext = viewModel;
+            };
+        }
+    }
+}
---
+++ WPFSeedKit/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bolareshet.WPFSeedKit.Interface;
+
+namespace Bolareshet.WPFSeedKit.ViewModel
+{
+    public class MainWindowViewModel : IMainWindowViewModel
+    {
+    }
+}
